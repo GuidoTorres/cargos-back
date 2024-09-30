00a9738c1891eacca44a3de87aef0d8d036e6679
Hector <gt12930@GMAIL.com>
2024-09-30 10:15:00 -0500

    feature: consulta bienes siga

--- src/routes/bienesInventario.js
+++ src/routes/bienesInventario.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const bienes = require("../controllers/bienesInventario");
 
-router.get("/", bienes.getBienes)
+router.get("/", bienes.getConsultaBienesSiga)
 router.get("/prueba", bienes.getBienesPrueba)
 
 
