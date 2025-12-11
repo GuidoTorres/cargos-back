e0cb3cca9bc0e020e4f8974de2267a81345bf94d
Hector <gt12930@GMAIL.com>
2025-12-11 17:21:24 -0500

    fix

--- src/controllers/personal.js
+++ src/controllers/personal.js
@@ -2,7 +2,7 @@ const { models } = require("./../../config/database");
 
 const getSigPersonal = async (req, res, next) => {
   try {
-    const get = await models.sig_personal.findAll({attributes:["empleado", "docum_ident", "nombre_completo"]});
+    const get = await models.sig_personal.findAll({attributes:["empleado", "docum_ident", "nombre_completo", "estado"]});
     return res.status(200).json({ data: get });
   } catch (error) {
     res.status(500).json();
